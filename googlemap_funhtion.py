a9a71ea57f09b7497d3cdaf72c461fc86a739a65
ericcc8888 <eric841015111@gmail.com>
2024-08-19 01:54:41 +0800

    fix textmessage and location message

--- googlemap_funhtion.py
+++ googlemap_funhtion.py
@@ -1,26 +1,3 @@
-# import googlemaps   ##導入googlemaps模組
-
-# gmaps = googlemaps.Client(key='AIzaSyANzenmkUxcDLd7jGwvo676-qD-8YdMoM0') ##利用API建立客戶端
-
-# # Geocoding an address
-# geocode_result = gmaps.geocode('Taiwan')[0] ##利用Geocode函數進行定位
-# location = geocode_result['geometry']['location'] #取得定位後經緯度
-
-# print(location)
-
-
-# #location回傳格式如下圖，可依照格式自訂義變數，就不用調用Geodcode API
-
-# # Search for places
-# #(keyword參數="輸入你想查詢的物件",radius="公尺單位")
-
-# places_result = gmaps.places_nearby(location,type='restaurant', radius=50000)
-
-# ##列印出地點的名稱與地點
-# for place in places_result['results']:
-#     print(place['name'], place['vicinity']) 
-
-
 from flask import Flask, request, abort
 
 from linebot.v3 import (
@@ -38,12 +15,9 @@ from linebot.v3.messaging import (
 )
 from linebot.v3.webhooks import (
     MessageEvent,
-    TextMessageContent
+    TextMessageContent,
+    LocationMessageContent
 )
-
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, LocationMessage,TextSendMessage
 
 import os
 import sys
@@ -61,8 +35,6 @@ if channel_access_token is None:
     sys.exit(1)
 
 handler = WebhookHandler(channel_secret)
-
-line_bot_api = LineBotApi(channel_access_token)
 
 configuration = Configuration(
     access_token=channel_access_token
@@ -98,11 +70,12 @@ def handle_message(event):
             )
         )
 
-# 处理 LocationMessage 事件
-@handler.add(MessageEvent, message=LocationMessage)
+# # 处理 LocationMessage 事件
+@handler.add(MessageEvent, message=LocationMessageContent)
 def handle_location_message(event):
-    # 从事件中提取位置信息
+    # 从事件中提取位置信息W
     location = event.message
+    print(location)
 
     # 打印位置数据
     print(f"地点名称: {location.title}")
@@ -112,8 +85,15 @@ def handle_location_message(event):
 
     # 可以根据需求，做其他处理，比如回复用户消息
     reply_message = f"你的位置: {location.title} ({location.address})\n纬度: {location.latitude}\n经度: {location.longitude}"
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=reply_message)]
+            )
+        )
 
 if __name__ == "__main__":
     app.run(debug=True)
